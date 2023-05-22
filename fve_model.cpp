c0c5bdd0acc82ccd8a00be0208e6668b1f3219f6
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-05-22 12:06:39 -0700

    Staging buffers done, beginning model loading

--- fve_model.cpp
+++ fve_model.cpp
@@ -1,44 +1,131 @@
 #include "fve_model.hpp"
 
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
 #include <cassert>
+#include <limits>
 
 namespace fve {
 
-	FveModel::FveModel(FveDevice& device, const std::vector<Vertex>& vertices) : device{ device } {
-		createVertexBuffers(vertices);
+	FveModel::FveModel(FveDevice& device, const FveModel::Builder& builder) : device{ device } {
+		createVertexBuffers(builder.vertices);
+		createIndexBuffers(builder.indices);
 	}
 
 	FveModel::~FveModel() {
 		vkDestroyBuffer(device.device(), vertexBuffer, nullptr);
 		vkFreeMemory(device.device(), vertexBufferMemory, nullptr);
+		if (hasIndexBuffer) {
+			vkDestroyBuffer(device.device(), indexBuffer, nullptr);
+			vkFreeMemory(device.device(), indexBufferMemory, nullptr);
+		}
 	}
 
 	void FveModel::createVertexBuffers(const std::vector<Vertex>& vertices) {
+		// count the vertices, veryfi we have at least 3
 		vertexCount = static_cast<uint32_t>(vertices.size());
 		assert(vertexCount >= 3 && "Vertex count must be at least 3");
+
+		// compute the size of the buffer we need
 		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+
+		// TODO: automatically use uint16_t for simple models
+		complexModel = vertexCount > std::numeric_limits<uint16_t>::max();
+
+		// create a staging buffer
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
 
 		device.createBuffer(
 			bufferSize,
-			VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+			VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer,
+			stagingBufferMemory);
+
+		// copy the vertex data into the staging buffer
+		void* data;
+		vkMapMemory(device.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(device.device(), stagingBufferMemory);
+
+		// create a device local buffer on the GPU
+		device.createBuffer(
+			bufferSize,
+			VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 			vertexBuffer,
 			vertexBufferMemory);
 
+		// copy the staging buffer contents into the device local buffer
+		device.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+		// destroy the staging buffer as we no longer need it
+		vkDestroyBuffer(device.device(), stagingBuffer, nullptr);
+		vkFreeMemory(device.device(), stagingBufferMemory, nullptr);
+	}
+
+	void FveModel::createIndexBuffers(const std::vector<uint32_t>& indices) {
+		// count the indices, determine if we're using an index buffer for this model
+		indexCount = static_cast<uint32_t>(indices.size());
+		hasIndexBuffer = indexCount > 0;
+
+		// if we have no indices, this model is not using an index buffer
+		if (!hasIndexBuffer) return;
+
+		// compute the size of the buffer we need
+		VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+
+		// create a staging buffer
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+
+		device.createBuffer(
+			bufferSize,
+			VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer,
+			stagingBufferMemory);
+
+		// copy the index data into the staging buffer
 		void* data;
-		vkMapMemory(device.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
-		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-		vkUnmapMemory(device.device(), vertexBufferMemory);
+		vkMapMemory(device.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(device.device(), stagingBufferMemory);
+
+		// create a device local buffer on the GPU
+		device.createBuffer(
+			bufferSize,
+			VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+			indexBuffer,
+			indexBufferMemory);
+
+		// copy the staging buffer contents into the device local buffer
+		device.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+		// destroy the staging buffer as we no longer need it
+		vkDestroyBuffer(device.device(), stagingBuffer, nullptr);
+		vkFreeMemory(device.device(), stagingBufferMemory, nullptr);
 	}
 
 	void FveModel::draw(VkCommandBuffer commandBuffer) {
-		vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+		if (hasIndexBuffer) {
+			vkCmdDrawIndexed(commandBuffer, indexCount, 1, 0, 0, 0);
+		}
+		else {
+			vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+		}
 	}
 
 	void FveModel::bind(VkCommandBuffer commandBuffer) {
 		VkBuffer buffers[] = { vertexBuffer };
 		VkDeviceSize offsets[] = { 0 };
 		vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+		if (hasIndexBuffer) {
+			vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+		}
 	}
 
 	std::vector<VkVertexInputBindingDescription> FveModel::Vertex::getBindingDescriptions() {
--- fve_model.hpp
+++ fve_model.hpp
@@ -12,14 +12,21 @@ namespace fve {
 	public:
 
 		struct Vertex {
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position{};
+			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> geAttributeDescriptions();
 		};
 
-		FveModel(FveDevice& device, const std::vector<Vertex>& vertices);
+		struct Builder {
+			std::vector<Vertex> vertices{};
+			std::vector<uint32_t> indices{};
+		};
+
+		FveModel(FveDevice& device, const FveModel::Builder& builder);
 		~FveModel();
 
 		FveModel(const FveModel&) = delete;
@@ -30,11 +37,19 @@ namespace fve {
 
 	private:
 		FveDevice& device;
+
 		VkBuffer vertexBuffer;
 		VkDeviceMemory vertexBufferMemory;
 		uint32_t vertexCount;
 
+		bool hasIndexBuffer = false;
+		bool complexModel = false;
+		VkBuffer indexBuffer;
+		VkDeviceMemory indexBufferMemory;
+		uint32_t indexCount;
+
 		void createVertexBuffers(const std::vector<Vertex>& vertices);
+		void createIndexBuffers(const std::vector<uint32_t>& indices);
 	};
 
 }
